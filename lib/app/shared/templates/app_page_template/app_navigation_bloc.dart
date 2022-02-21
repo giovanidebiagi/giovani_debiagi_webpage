a613703e763edc8df22d793313759abbedc5629f
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-02-21 17:54:13 -0300

    Turned _currentInnerRouteStreamController into a broadcast controller. This is needed since every time AppNavigationBarNavigationButtons widget is rebuilt (due to MediaQuery object used by its width), a new StreamBuilder listens to the stream. Also created dispose() method for closing that stream.

--- lib/app/shared/templates/app_page_template/app_navigation_bloc.dart
+++ lib/app/shared/templates/app_page_template/app_navigation_bloc.dart
@@ -4,10 +4,14 @@ import 'package:flutter/material.dart';
 class AppNavigationBloc {
   final innerNavigatorKey = GlobalKey<NavigatorState>();
 
-  final _currentInnerRouteStreamController = StreamController<String>()..add('/home');
+  final _currentInnerRouteStreamController = StreamController<String>.broadcast()..add('/home');
   Stream<String> get currentInnerRouteStream => _currentInnerRouteStreamController.stream;
 
   setCurrentInnerRoute(String routeName) {
     _currentInnerRouteStreamController.add(routeName);
+  }
+
+  dispose() {
+    _currentInnerRouteStreamController.close();
   }
 }
