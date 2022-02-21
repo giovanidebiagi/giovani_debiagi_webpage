cbe74c1102f98b1191fcce8cee97736c98e63ffc
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-02-21 17:53:25 -0300

    Changed App class to StatefulWidget for closing AppNavigationBloc _currentInnerRouteStreamController, using dispose().

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,18 +3,29 @@ import 'package:giovani_debiagi_webpage/app/shared/templates/app_page_template/a
 import 'package:giovani_debiagi_webpage/app/shared/templates/app_page_template/app_page_template.dart';
 import 'shared/constants/app_theme.dart';
 
-class App extends StatelessWidget {
-  App({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
 
-  final _navigationBloc = AppNavigationBloc();
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final _appNavigationBloc = AppNavigationBloc();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.getThemeData(context),
       home: AppPageTemplate(
-        navigationBloc: _navigationBloc,
+        appNavigationBloc: _appNavigationBloc,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _appNavigationBloc.dispose();
+    super.dispose();
   }
 }
