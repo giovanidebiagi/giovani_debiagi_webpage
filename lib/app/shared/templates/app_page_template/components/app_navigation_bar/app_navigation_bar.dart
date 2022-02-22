d2ff1e101734bab28046cf05517628ead622a637
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-02-22 10:50:40 -0300

    Changed package path on import

--- lib/app/shared/templates/app_page_template/components/app_navigation_bar/app_navigation_bar.dart
+++ lib/app/shared/templates/app_page_template/components/app_navigation_bar/app_navigation_bar.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import '../../../../constants/app_theme.dart';
 import '../../app_navigation_bloc.dart';
 import 'components/app_navigation_bar_crypto_quotation_widget/app_navigation_bar_crypto_quotation_widget.dart';
-import 'components/app_navigation_bar_navigation_buttons_widget.dart';
+import 'components/app_navigation_bar_navigation_buttons_widget/app_navigation_bar_navigation_buttons_widget.dart';
 import 'constants/app_navigation_bar_size_constants.dart';
 
 class AppNavigationBar extends StatelessWidget {
