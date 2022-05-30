543c6189f42cebf96db6c6cd227db37824eac3b4
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-05-30 15:09:48 -0300

    Implemented CacheException and CacheFailure.

---
+++ lib/core/errors/exceptions/cache_exception.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+import 'package:giovani_debiagi_webpage/core/errors/exceptions/i_exception.dart';
+
+class CacheException extends Equatable implements IException {
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/core/errors/failures/cache_failure.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+import 'package:giovani_debiagi_webpage/core/errors/failures/i_failure.dart';
+
+class CacheFailure extends Equatable implements IFailure {
+  @override
+  List<Object?> get props => [];
+}
