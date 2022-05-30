f150d6bae0e96586a3afb0169fb100794c7039fc
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-05-30 11:36:51 -0300

    Created PersonalInfo and Skill models.

---
+++ lib/features/home/domain/entities/personal_info.dart
@@ -0,0 +1,18 @@
+class PersonalInfo {
+  String name;
+  String lastName;
+  String email;
+  String about;
+  String gitHubUrl;
+  String linkedInUrl;
+  String youTubeUrl;
+  PersonalInfo({
+    required this.name,
+    required this.lastName,
+    required this.email,
+    required this.about,
+    required this.gitHubUrl,
+    required this.linkedInUrl,
+    required this.youTubeUrl,
+  });
+}
---
+++ lib/features/home/domain/entities/skill.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Skill {
+  String name;
+  Image icon;
+  Skill({
+    required this.name,
+    required this.icon,
+  });
+}
