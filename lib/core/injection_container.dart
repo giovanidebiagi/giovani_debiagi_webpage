3f2e4cecfb70687bc7ef18b91e6bcaf1a2f88fa4
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-06-03 19:15:46 -0300

    Dependency injection (service locator -> get_it)

---
+++ lib/core/injection_container.dart
@@ -0,0 +1,31 @@
+import 'package:get_it/get_it.dart';
+import 'package:giovani_debiagi_webpage/features/home/data/datasources/i_personal_data_local_datasource.dart';
+import 'package:giovani_debiagi_webpage/features/home/data/datasources/personal_data_local_datasource.dart';
+import 'package:giovani_debiagi_webpage/features/home/data/repositories/personal_data_repository.dart';
+import 'package:giovani_debiagi_webpage/features/home/domain/usecases/get_skills.dart';
+import 'package:giovani_debiagi_webpage/features/home/presentation/blocs/get_skills_bloc.dart';
+
+import '../features/home/domain/repositories/i_personal_data_repository.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> init() async {
+//! Features
+// Datasources
+  getIt.registerLazySingleton<IPersonalDataLocalDatasource>(
+      () => PersonalDataLocalDatasource());
+
+// Repositories
+  getIt.registerLazySingleton<IPersonalDataRepository>(
+      () => PersonalDataRepository(personalDataLocalDatasource: getIt()));
+
+// Usecases
+  getIt.registerLazySingleton(() => GetSkills(repository: getIt()));
+
+// BLoCs
+  getIt.registerFactory(() => GetSkillsBloc(getSkillsUsecase: getIt()));
+
+//! External
+
+//! Core
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:giovani_debiagi_webpage/core/injection_container.dart'
+    as injection_container;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await injection_container.init();
+
   runApp(const App());
 }
