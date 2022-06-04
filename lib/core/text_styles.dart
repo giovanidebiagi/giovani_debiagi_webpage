ca302aa6987eb84567de29422c7fe8762aa09bdf
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-06-04 15:11:14 -0300

    Created TextStyles class.

---
+++ lib/core/text_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:giovani_debiagi_webpage/core/size_constants.dart';
+
+class TextStyles {
+  static const TextStyle sectionTitleTextStyle =
+      TextStyle(fontSize: SizeConstants.sectionTitleTextSize, fontWeight: FontWeight.bold);
+}
