070e75db4cf229bf7e2c6caf576783520929a58e
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-06-22 12:16:55 -0300

    Added Firebase config for using Firebase Hosting.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'package:giovani_debiagi_webpage/core/injection_container.dart'
-    as injection_container;
+import 'package:giovani_debiagi_webpage/core/injection_container.dart' as injection_container;
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await injection_container.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const App());
 }
