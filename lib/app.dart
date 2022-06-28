c21f6824b51311c63c0a69a3790211c0650f5a0c
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-06-28 16:01:47 -0300

    Changed page icon and title.

--- lib/app.dart
+++ lib/app.dart
@@ -11,6 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Giovani Debiagi Portfolio',
       theme: buildTheme(Brightness.dark),
       onGenerateRoute: (settings) {
         Widget? _page;
