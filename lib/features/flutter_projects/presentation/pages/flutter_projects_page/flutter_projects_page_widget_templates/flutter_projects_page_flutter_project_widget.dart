543c964af9dcdd66814ebbf19585d014a6668b59
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-06-28 16:02:54 -0300

    Removed commented code. Fixed typo.

--- lib/features/flutter_projects/presentation/pages/flutter_projects_page/flutter_projects_page_widget_templates/flutter_projects_page_flutter_project_widget.dart
+++ lib/features/flutter_projects/presentation/pages/flutter_projects_page/flutter_projects_page_widget_templates/flutter_projects_page_flutter_project_widget.dart
@@ -52,7 +52,7 @@ class FlutterProjectsPageFlutterProjectWidget extends StatelessWidget {
                           onPressed: () async {
                             await launchUrlString(flutterProject.gitHubUrl);
                           },
-                          child: const Text('Avaliable on GitHub'),
+                          child: const Text('Available on GitHub'),
                         ),
                       ),
                     ],
@@ -79,87 +79,5 @@ class FlutterProjectsPageFlutterProjectWidget extends StatelessWidget {
         ),
       ),
     );
-    // return Column(
-    //   children: [
-    //     Container(
-    //       color: color,
-    //       child: Padding(
-    //         padding:
-    //             const EdgeInsets.symmetric(vertical: 48.0, horizontal: 48.0),
-    //         child: Column(
-    //           children: [
-    //             SizedBox(
-    //               width: 4 * SizeConstants.flutterProjectsScreenshotsWidth +
-    //                   3 *
-    //                       SizeConstants
-    //                           .flutterProjectsHorizontalSpacingBetweenScreenshots,
-    //               child: Column(
-    //                 children: [
-    //                   Row(
-    //                     mainAxisAlignment: MainAxisAlignment.center,
-    //                     children: [
-    //                       Text(
-    //                         flutterProject.name,
-    //                         style: TextStyles.sectionTitleTextStyle,
-    //                       ),
-    //                       flutterProject.gitHubUrl != ''
-    //                           ? Row(
-    //                               children: [
-    //                                 const SizedBox(width: 24.0),
-    //                                 MouseRegion(
-    //                                   cursor: SystemMouseCursors.click,
-    //                                   child: ElevatedButton(
-    //                                     style: ButtonStyle(
-    //                                       backgroundColor:
-    //                                           MaterialStateProperty.all(
-    //                                               Colors.black),
-    //                                     ),
-    //                                     onPressed: () async {
-    //                                       await launchUrlString(
-    //                                           flutterProject.gitHubUrl);
-    //                                     },
-    //                                     child:
-    //                                         const Text('Avaliable on GitHub'),
-    //                                   ),
-    //                                 ),
-    //                               ],
-    //                             )
-    //                           : const SizedBox.shrink()
-    //                     ],
-    //                   ),
-    //                   const SizedBox(
-    //                       height: SizeConstants
-    //                           .spacingBetweenProjectTitleAndDescription),
-    //                   Text(
-    //                     flutterProject.description,
-    //                     textAlign: TextAlign.center,
-    //                   ),
-    //                   const SizedBox(height: 36.0),
-    //                   Wrap(
-    //                     direction: Axis.horizontal,
-    //                     runSpacing: SizeConstants
-    //                         .flutterProjectsVerticalSpacingBetweenScreenshots,
-    //                     spacing: SizeConstants
-    //                         .flutterProjectsHorizontalSpacingBetweenScreenshots,
-    //                     children: flutterProject.projectImagesPaths
-    //                         .map(
-    //                           (projectImagePath) => Image.asset(
-    //                             projectImagePath,
-    //                             width: SizeConstants
-    //                                 .flutterProjectsScreenshotsWidth,
-    //                             filterQuality: FilterQuality.medium,
-    //                           ),
-    //                         )
-    //                         .toList(),
-    //                   ),
-    //                 ],
-    //               ),
-    //             )
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-    //   ],
-    // );
   }
 }
