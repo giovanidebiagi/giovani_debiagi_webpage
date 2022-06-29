80b8cf39d1fb5696a51b0c3cf39a152c52353771
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-06-29 09:31:09 -0300

    Using url_strategy package for removing # from url.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'app.dart';
 import 'core/injection_container.dart' as injection_container;
 import 'firebase_options.dart';
@@ -7,6 +8,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  setPathUrlStrategy();
   await injection_container.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
